2b54dc5c6b0541f6896918615c859a51b827cbff
krote <krote0924@gmail.com>
2022-07-16 17:58:44 +0900

    all recture is done

--- routes/countdown.tsx
+++ routes/countdown.tsx
@@ -1,13 +1,13 @@
-/** h */
+/** @jsx h */
 import { h } from "preact";
-import Countdown from "../islands/Countdown";
+import Countdown from "../islands/Countdown.tsx";
 
 export default function Page() {
     const date = new Date();
     date.setHours(date.getHours() + 1);
     return (
         <p>
-            The big event is happening<Countdown target={date.toISOString()} />.
+            The big event is happening <Countdown target={date.toISOString()} />.
         </p>
     );
 }
