1486a953f2e4f842632f6baf106f63c5f68e2784
krote <krote0924@gmail.com>
2022-07-18 23:51:45 +0900

    add ex tutorial

---
+++ routes/blog.tsx
@@ -0,0 +1,54 @@
+/** @jsx h */
+import { h, PageProps } from "preact";
+import { Head } from "$fresh/src/runtime/head.ts";
+import { Handler } from "$fresh/server.ts";
+
+interface Article{
+    id: string;
+    title: string;
+    created_at: string;
+}
+
+export const handler: Handlers<Article[]> = {
+    async GET(_, ctx) {
+        const articles: Article[] = [
+            {
+                id: "1",
+                title: "Article 1",
+                created_at: "2022-07-17T00:00:00.000Z",
+            },
+            {
+                id: "2",
+                title: "Article 2",
+                created_at: "2022-07-18T00:00:00.000Z",
+            },
+        ];
+        return ctx.render(articles);
+    },
+}
+
+export default function Home({ data }: PageProps<Article[]>) {
+    return (
+        <div>
+            <Head>
+                <title>Fresh Blog</title>
+            </Head>
+            <div>
+                <h1>Fresh Blog</h1>
+                <section>
+                    <h2>Posts</h2>
+                    <ul>
+                        {data.map((article) => (
+                            <li key={article.id}>
+                                <a href={`articles/${article.id}`}>
+                                    <h3>{article.title}</h3>
+                                    <time dateTime={article.created_at}>{article.created_at}</time>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            </div>
+        </div>
+    );
+}
